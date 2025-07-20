8172d2386f2ff837db224b16d4597f358c8974aa
BenBurke <40806608+BenLBurke@users.noreply.github.com>
2025-07-20 17:14:13 -0400

    Update config.js
    Updated event colors

--- config/config.js
+++ config/config.js
@@ -64,7 +64,7 @@ let config = {
 							},
 							{	url: "https://calendar.google.com/calendar/ical/mutkcai83gusm8ek7o6nj00cr4%40group.calendar.google.com/public/basic.ics",
 								name: "burke_calendar",
-								color: "blue"
+								color: "#1e90ff"
 							}
 					]
 			}
