dfdd55f1471243ab4bdfeec7718cf4479c785aba
Ben Burke <benleeburke@gmail.com>
2026-01-07 23:57:11 -0500

    updating default compliments to be kid-friendly

--- modules/default/compliments/compliments.js
+++ modules/default/compliments/compliments.js
@@ -4,10 +4,10 @@ Module.register("compliments", {
 	// Module config defaults.
 	defaults: {
 		compliments: {
-			anytime: ["Hey there sexy!"],
-			morning: ["Good morning, handsome!", "Enjoy your day!", "How was your sleep?"],
-			afternoon: ["Hello, beauty!", "You look sexy!", "Looking good today!"],
-			evening: ["Wow, you look hot!", "You look nice!", "Hi, sexy!"],
+			anytime: ["Focus on the Temple."],
+			morning: ["Who will you serve today?", "Enjoy your day!", "How was your sleep?"],
+			afternoon: ["You got this!", "Make sure you eat lunch today", "Looking good today!"],
+			evening: ["Keep it up!", "You deserve some rest.", "Nice work today"],
 			"....-01-01": ["Happy new year!"]
 		},
 		updateInterval: 30000,
